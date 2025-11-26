e0217abc508588b794d90369aaddb1ac82daa035
mochamap1e <mcmicrosoft5@example.com>
2025-11-26 15:17:41 -0500

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
-import clsx from "clsx";
-import { useState, useRef } from "react";
-
-import gsap from "./utils/gsap";
-import Startup from "./components/Startup";
-
 export default function App() {
-  const [startupComplete, setStartupComplete] = useState(false);
-
-  const overlayRef = useRef(null);
-
-  gsap.to(overlayRef.current, { opacity: 1 });
-
   return (
-    <div className={clsx(
-        "bg-[linear-gradient(#cccccc_0%,#dedfdf_50%,#cccccc_100%)]",
-        "flex items-center justify-center")}>
-
-      {startupComplete ? <h1>hi</h1> : <Startup onComplete={() => setStartupComplete(true)} /> }
-      <div ref={overlayRef} className="fixed bg-black w-screen h-screen z-1"></div>
+    <div>
+      <h1>Hi</h1>
     </div>
   )
 }
--- src/utils/gsap.js
+++
@@ -1,6 +0,0 @@
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-
-gsap.registerPlugin(useGSAP);
-
-export default gsap;
