8092af0be95b9fb3224e444940e6d4a215a32096
mochamap1e <mcmicrosoft5@example.com>
2025-11-26 13:16:59 -0500

    broken code

--- src/components/Startup.jsx
+++ src/components/Startup.jsx
@@ -1,38 +1,67 @@
-import clsx from "clsx";
 import { useEffect, useState, useRef } from "react";
 
-import gsap from "../utils/gsap";
+export default function Startup({ onComplete }) {
+    const audioDisabledPreference = localStorage.getItem("startupAudioDisabled") === "true";
 
-export default function Startup() {
     const videoRef = useRef(null);
-    const overlayRef = useRef(null);
 
-    const [buttonVisible, setButtonVisible] = useState(true);
+    const [optionsVisible, setOptionsVisible] = useState(true);
+    const [audioDisabled, setAudioDisabled] = useState(audioDisabledPreference || false);
 
-    const sound = new Audio("/sfx/startup.mp3"); sound.preload = "auto";
+    const soundRef = useRef(new Audio("/sfx/startup.mp3"));
+    soundRef.current.preload = "auto";
+
+    function toggleMute(value) {
+        setAudioDisabled(value);
+        localStorage.setItem("startupAudioDisabled", value);
+    }
 
     useEffect(() => {
-        videoRef.current.addEventListener("ended", () => {
-            gsap.to(overlayRef.current, { opacity: 1 });
-        });
-    }, []);
+        const video = videoRef.current;
+        const sound = soundRef.current;
+
+        let videoEnded = false;
+        const onEnded = () => {
+            videoEnded = true;
+            
+            onComplete();
+        }
+        if (video) video.addEventListener("ended", onEnded);
+
+        const onVisibilityChange = () => {
+            if (document.hidden) {
+                video.pause();
+                sound.pause();
+            } else {
+                if (!videoEnded) {
+                    video.play();
+                    if (!audioDisabled) sound.play();
+                }
+            }
+        }
+        document.addEventListener("visibilitychange", onVisibilityChange);
+        
+        return () => {
+            if (video) video.removeEventListener("ended", onEnded);
+            document.removeEventListener("visibilitychange", onVisibilityChange);
+        }
+    }, [audioDisabled]);
 
     return (
-        <div className={clsx(
-            "bg-[linear-gradient(#cccccc_0%,#dedfdf_50%,#cccccc_100%)]",
-            "flex items-center justify-center")}>
-            
-            {buttonVisible && <button className="fixed text-white text-2xl cursor-pointer z-3" onClick={() => {
-                setButtonVisible(false);
+        <div className="flex flex-col items-center justify-center gap-5 z-2">
+            {optionsVisible &&
+            <div>
+                <button className="text-white text-2xl cursor-pointer" onClick={() => {
+                    setOptionsVisible(false);
+                    videoRef.current.play();
+                    if (!audioDisabled) soundRef.current.play();
+                }}>play</button>
+                
+                <label className="text-white">mute? <input type="checkbox" checked={audioDisabled} onChange={(e) => toggleMute(e.target.checked)} /></label>
+            </div>
+            }
 
-                videoRef.current.play();
-                sound.play();
-
-                gsap.to(overlayRef.current, { opacity: 0, delay: 0.2 });
-            }}>click</button>}
-
-            <video muted preload="auto" ref={videoRef} src="/videos/startup.mp4" className="w-screen h-screen z-1"></video>
-            <div ref={overlayRef} className="fixed bg-black w-screen h-screen z-2"></div>
+            <video muted preload="auto" ref={videoRef} src="/videos/startup.mp4" className="w-screen h-screen z-0"></video>
         </div>
     );
 }
