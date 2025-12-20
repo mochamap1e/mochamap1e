b943a5250bf9164cb5fe91030436ba1e0bc71b41
mochamap1e <mcmicrosoft5@example.com>
2025-12-20 00:32:25 -0500

    omg matter

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,33 @@
-import { Octokit } from "octokit";
-import { useState, useEffect } from "react";
+import { useRef, useEffect } from "react";
+import Matter from "matter-js";
 
 import "./styles.css";
-import Link from "./components/Link";
 
 export default function App() {
-    ////////// BIO //////////
-
-    const octokit = new Octokit();
-
-    const storageKey = "cachedBio";
-    const [bioText, setBioText] = useState("fetching...");
-
-    async function requestCurrentBio() {
-        try {
-            const { data: { bio: response } } = await octokit.request("GET /users/mochamap1e");
-
-            setBioText(response);
-            localStorage.setItem(storageKey, JSON.stringify({ text: response, timestamp: Date.now() }));
-        } catch(error) {
-            setBioText("failed to fetch bio");
-            console.error(error);
-        }
-    }
+    const canvasRef = useRef(null);
 
     useEffect(() => {
-        const storedBio = localStorage.getItem(storageKey);
+        const canvas = canvasRef.current; if (!canvas) return;
 
-        if (storedBio) {
-            const parsedStoredBio = JSON.parse(storedBio);
+        const Engine = Matter.Engine,
+            Render = Matter.Render,
+            Runner = Matter.Runner,
+            Bodies = Matter.Bodies,
+            Composite = Matter.Composite
+        
+        const engine = Engine.create();
+        const render = Render.create({ canvas: canvas, engine: engine });
 
-            if ((Date.now() - parsedStoredBio.timestamp) >= 3600000) { // 1 hour
-                requestCurrentBio();
-            } else {
-                setBioText(parsedStoredBio.text);
-            }
-        } else {
-            requestCurrentBio();
-        }
-    }, []);
-
-    ////////// PAGE //////////
+        Composite.add(engine.world, [
+            Bodies.rectangle(400, 200, 80, 80),
+            Bodies.rectangle(400, 610, 810, 60, { isStatic: true })
+        ]);
+        
+        Render.run(render);
+        Runner.run(Runner.create(), engine);
+    }, [])
 
     return (
-        <div id="page-container" className="container">
-            <div id="info-container" className="container">
-                <img
-                    src="https://github.com/mochamap1e.png?size=256"
-                    draggable="false"
-                />
-
-                <div>
-                    <h1>mochamap1e</h1>
-                    <p>{bioText}</p>
-                </div>
-            </div>
-
-            <div id="links-container" className="container">
-                <Link>projects</Link>
-                <Link>socials</Link>
-                <Link>setup</Link>
-            </div>
-        </div>
+        <canvas ref={canvasRef}/>
     )
 }
