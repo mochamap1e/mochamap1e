7fec473945982041af2ef9e354d00cc75d0149ee
mochamap1e <mcmicrosoft5@example.com>
2026-07-01 21:07:35 -0400

    add last.fm

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,7 @@ export default function App() {
                         <Social title="YouTube" image="youtube.svg" url="https://www.youtube.com/@mocchamaple"/>
                         <Social title="Twitch" image="twitch.svg" url="https://www.twitch.tv/mocchamaple"/>
                         <Social title="TikTok" image="tiktok.svg" url="https://www.tiktok.com/@mochamap1e"/>
+                        <Social title="Last.fm" image="lastfm.svg" url="https://www.last.fm/user/mocchamaple"/>
                         <Social title="Steam" image="steam.svg" url="https://steamcommunity.com/id/mochamap1e"/>
                         <Social title="Geometry Dash" image="gdlb.png" url="https://gdbrowser.com/u/mochamaple"/>
                         <Social title="GD Demon Ladder" image="gddl.png" url="https://gdladder.com/profile/55942"/>
